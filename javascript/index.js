aa213566c12f63ab85eb3ebb6cb5d2b3d70ad39d
Gayatri88-art <gayatriivarade@gmail.com>
2025-01-18 09:29:05 +0530

    basic concept related to js

---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+console.log("gargi");
+console.log("my name is gargiiiiii");
+
+let a = 5;
+let b = 10;
+console.log("SUM:",a+b);
+
+let firstprice = 56;
+let secondprice = 10;
+console.log(`the total price is:${firstprice + secondprice} rs.`); //template literals
+
+//operators
+let age = 18;
+console.log(age>18); //false
+console.log(age>=18); //true
+
