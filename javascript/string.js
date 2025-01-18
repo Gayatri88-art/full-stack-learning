7a0a91b99c927b599e26686a7dd15b1a8fdc93cd
Gayatri88-art <gayatriivarade@gmail.com>
2025-01-18 17:00:50 +0530

    touppercase and to lowercase method

--- javascript/string.js
+++ javascript/string.js
@@ -2,4 +2,10 @@
  let newpassword = password.trim();  //trim is use to remove un-neccessary space from starting and ending.
  console.log(newpassword);
 
- 
+ //strings are immutable in js --> no changes can be made to string.(is we try to create new string then new string and old one both exist)
+
+let name = "gayatri";
+let msg = "Error";
+console.log( msg.toUpperCase() );
+console.log( msg.toLowerCase() );
+
