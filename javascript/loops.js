984fccbebe3389d66f936d62708142cd6c3e9ed7
Gayatri88-art <gayatriivarade@gmail.com>
2025-01-21 22:14:36 +0530

    simple codes related to for loop

--- javascript/loops.js
+++ javascript/loops.js
@@ -1,8 +1,30 @@
 
-for( let i=1;i<=6;i++){
-  console.log(i);
-}
+// for( let i=1;i<=6;i++){
+//   console.log(i);
+// }
 
-for(let i = 1; i<=15; i%2!=0){
+// for(let i = 1; i<=15; i = i+2){
+//   console.log(i);
+// }
+
+// console.log("backward:")
+// for(let i = 15; i>=1; i = i-2){
+//   console.log(i);
+// }
+
+// for(let i = 2; i<=10; i = i*2){
+//   console.log(i);
+// }
+
+// for(let i = 2; i<=50; i = i+2){
+//   console.log(i);
+// }
+
+// for(let i = 5; i<=50; i = i+5){
+//   console.log(i);
+// }
+
+let n = prompt("enter your number:-");
+for( let i=n;i<=n*10;i=i+n){
   console.log(i);
 }
