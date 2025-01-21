992311ab42bf394fee3e4eabf03c10f536d10fd8
Gayatri88-art <gayatriivarade@gmail.com>
2025-01-21 22:52:09 +0530

    nested loops

--- javascript/loops.js
+++ javascript/loops.js
@@ -1,30 +1,37 @@
 
-// for( let i=1;i<=6;i++){
-//   console.log(i);
-// }
-
-// for(let i = 1; i<=15; i = i+2){
-//   console.log(i);
-// }
-
-// console.log("backward:")
-// for(let i = 15; i>=1; i = i-2){
-//   console.log(i);
-// }
-
-// for(let i = 2; i<=10; i = i*2){
-//   console.log(i);
-// }
-
-// for(let i = 2; i<=50; i = i+2){
-//   console.log(i);
-// }
-
-// for(let i = 5; i<=50; i = i+5){
-//   console.log(i);
-// }
-
-let n = prompt("enter your number:-");
-for( let i=n;i<=n*10;i=i+n){
+for( let i=1;i<=6;i++){
   console.log(i);
+}
+
+for(let i = 1; i<=15; i = i+2){
+  console.log(i);
+}
+
+console.log("backward:")
+for(let i = 15; i>=1; i = i-2){
+  console.log(i);
+}
+
+for(let i = 2; i<=10; i = i*2){
+  console.log(i);
+}
+
+for(let i = 2; i<=50; i = i+2){
+  console.log(i);
+}
+
+for(let i = 5; i<=50; i = i+5){
+  console.log(i);
+}
+
+// let n = prompt("enter your number:-");
+// n = parseInt(n);
+// for( let i=n;i<=n*10;i=i+n){
+//   console.log(i);
+// }
+
+for(let i=1;i<=5;i++){
+  for(let j=1;j<=6;j++){
+    console.log(j);
+  }
 }
