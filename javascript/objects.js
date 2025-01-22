9d8c2e1055ea59612f784a917cb940f70772cbd7
Gayatri88-art <gayatriivarade@gmail.com>
2025-01-22 22:33:19 +0530

    nested objects

--- javascript/objects.js
+++ javascript/objects.js
@@ -18,9 +18,36 @@
   gender:"female"
 
  };
+ console.log(list);
  
  list.name = "sahil";
+ console.log(list.name);
  list.city;
  list.city = "mumbai";
  list.marks = 99;
  delete list.name;
+
+
+ const stu_info = {
+  gayatri: {
+    age:20,
+    grade:"A",
+  },
+  sahil: {
+    age:20,
+    grade:"o",
+  },
+  mayuri: {
+    age:20,
+    grade:"o",
+  },
+  sanika: {
+    age:20,
+    grade:"A+",
+  },
+ }
+ console.log(stu_info);
+ 
+ 
+ console.log(stu_info.gayatri);
+ console.log( delete stu_info.gayatri.age);
