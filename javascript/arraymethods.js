b703eb186cdc6a505937d30efbfdf51b32e54de0
Gayatri88-art <gayatriivarade@gmail.com>
2025-01-25 14:30:37 +0530

    array methods

--- javascript/arraymethods.js
+++ javascript/arraymethods.js
@@ -1,3 +1,4 @@
+//for each
 let arr = [1,2,3,4,5];
  arr.forEach(element => {
   console.log(element);
@@ -26,4 +27,103 @@ let arr = [1,2,3,4,5];
          arr2.forEach((student)=>{
           console.log(student.marks);
          })
- 
+
+         let gpa = arr2.map((el)=>{
+          return el.marks/10;
+         })
+         console.log(gpa);
+
+//map
+
+list = [2,4,6,8,10];
+let double = list.map((el) =>{
+  return el*el;
+});
+console.log(double);
+
+//filter
+nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+let check = nums.filter((el)=>{
+  return el%2==0; //even=true odd=false
+})
+console.log(check);
+
+//every
+let number = [2,4,6].every((el)=>el%2==0);
+console.log(number);
+
+//some
+let xyz = [1,3,5,4].some((el)=>el%2==0);
+console.log(xyz); //because some even number is there i.e 4
+
+//reduce
+let mno = [1,2,3,4,5].reduce((result,el)=> result+el);
+console.log(mno); // its working is like loop kind of
+
+//find the maximum from an array using reduce
+
+let pqr = [1,2,3,4,5,6,7,8,9,11,12];
+let result =pqr.reduce((result,el)=>{
+  if(result<el){
+    return  el;
+  }
+  else{
+    return result;
+  }
+});
+console.log(result);
+
+
+//practice question
+
+//1. all numbers from array are multiple of 10 or not
+ abc = [10,22,30,40,50];
+let def =abc.every((el)=>el%10==0);
+console.log(def);
+
+//2.create a function to find the min number from array
+
+ghi = [1,2,3,4,5,6,7,8,9];
+let jkl = ghi.reduce((res,el)=>{
+  if (res>el){
+    return el;
+  }
+  else{
+    return res;
+  }
+});
+console.log(jkl);
+
+//default parameter
+function sum(a,b=4){
+  return a+b;
+
+};
+
+//spread
+
+ghi = [1,2,3,4,5,6,7,8,9];
+console.log(...ghi);
+console.log(Math.min(...ghi));
+
+console.log(..."apnacollege");
+
+//spread --> with array literals
+let odd = [1,3,5,7,9];
+let even = [2,4,6,8,10];
+let mix = [...even,...odd];
+console.log(mix);
+
+//-->with object literals
+const data ={
+  name:"gayatri",
+  age:20,
+  clg:"rcp"
+};
+
+let copydata = {...data};
+console.log(copydata);
+
+
+
+
