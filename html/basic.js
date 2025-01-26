4bd40ba0dd7c7591d41ea40f0395d95bd844cfa4
Gayatri88-art <gayatriivarade@gmail.com>
2025-01-26 10:07:16 +0530

    js connecting with html

---
+++ html/basic.js
@@ -0,0 +1,13 @@
+console.dir(document.getElementById("gayuu"));
+console.dir(document.getElementsByClassName("first"));
+console.dir(document.getElementsByTagName("p")[1].innerText="new content");
+console.dir(document.querySelector('p'));
+console.dir(document.querySelector('.second'));
+
+
+console.dir(document.querySelectorall("div li"));
+
+
+
+
+
