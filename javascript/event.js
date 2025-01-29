e8db993f14070ebde635c7c74f5ce92c1e85ef95
Gayatri88-art <gayatriivarade@gmail.com>
2025-01-29 11:25:23 +0530

    this in events listener

---
+++ javascript/event.js
@@ -0,0 +1,41 @@
+let h1 = document.querySelector('h1');
+let h3 = document.querySelector('h3');
+let btn = document.querySelector('button');
+let p = document.querySelector('p');
+
+function changecolor(){
+  console.dir(this.innerText);
+  this.style.backgroundColor = "green";
+}
+
+
+
+h1.addEventListener("click",changecolor());
+
+h3.addEventListener("click",changecolor());
+p.addEventListener("click",changecolor());
+btn.addEventListener("click",changecolor());
+
+
+
+//wrong practice-->> removing redundancy by creating function --->>as mention above
+
+
+// h1.addEventListener("click",function(){
+//   console.dir(this.innerText);
+//   this.style.backgroundColor = "green";
+// })
+
+// h3.addEventListener("click",function(){
+//   console.dir(this.innerText);
+//   this.style.backgroundColor = "green";
+// })
+// p.addEventListener("click",function(){
+//   console.dir(this.innerText);
+//   this.style.backgroundColor = "green";
+// })
+// btn.addEventListener("click",function(){
+//   console.dir(this.innerText);
+//   this.style.backgroundColor = "green";
+// })
+
