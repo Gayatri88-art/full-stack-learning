37929a88c5be760e34bf5987cf4078babbb5810f
Gayatri88-art <gayatriivarade@gmail.com>
2025-01-29 08:48:52 +0530

    dom events - onclick and onmouseenter

--- javascript/domevents.js
+++ javascript/domevents.js
@@ -1,6 +1,23 @@
-let btn = document.querySelector("button");
-console.dir(btn);
+let btns = document.querySelectorAll("button");
+console.dir(btns);
 
-btn.onclick = function(){
-  alert("button was clicked");
+//onclick
+//onmouseenter
+for(btn of btns){
+  btn.onclick = sayhello;
+  btn.onmouseenter = function(){
+    console.log("you clicked a button");
+  }
+  console.dir(btn);
+}
+// btn.onclick = function(){
+//   alert("button was clicked");
+// };
+
+function sayhello(){
+  alert("hellow!!");
 };
+
+//
+
+
