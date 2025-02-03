0d328c444d146f466fd046260d6f918f4743eaee
Gayatri88-art <gayatriivarade@gmail.com>
2025-02-03 13:43:19 +0530

    call stack

--- javascript/stack.js
+++ javascript/stack.js
@@ -1,1 +1,11 @@
 //stack -->> stack is LIFO (last in first out)
+function one(){
+  return 1;
+}
+function two(){
+  return one() + one();
+}
+function three(){
+ let ans =  two()+ one();
+ console.log(ans);
+}
