fa84eef31dd2c332724d33b56dd64facd94da6dc
Gayatri88-art <gayatriivarade@gmail.com>
2025-02-04 08:11:36 +0530

    promises

--- javascript/stack.js
+++ javascript/stack.js
@@ -9,3 +9,35 @@ function three(){
  let ans =  two()+ one();
  console.log(ans);
 }
+three();
+
+//js is single threaded language
+setTimeout(()=>{
+  console.log("apna college")
+
+},2000);
+console.log("hiiiiiiiu");
+
+
+//callback nesting-->>callback hell
+let h1 = document.querySelector("h1");
+function colorchange(color,delay,nextcolor){
+  setTimeout(() =>{
+    h1.style.color = color;
+    if(nextcolor)nextcolor();
+  },delay);
+}
+colorchange("red",1000,()=>{
+  colorchange("green",1000,()=>{
+    colorchange("pink",1000,()=>{
+      colorchange("blue",1000,()=>{
+        colorchange("orange",1000,()=>{
+
+        });
+      });
+    });
+  });
+});
+
+//the promises object represent the eventual completion or failure od asyncronous operation and its resultion value.
+ 
