3fc2449737ecc4357f763161e67ac967bbaf6aae
Gayatri88-art <gayatriivarade@gmail.com>
2025-02-04 11:29:28 +0530

    refactoring old code

--- javascript/stack.js
+++ javascript/stack.js
@@ -39,6 +39,36 @@
 //   });
 // });
 
+//above code in simple way or you can understand this code more easily
+function colorchange(color,delay,nextcolor){
+  return new Promise((resolve,reject)=>{
+    setTimeout(() =>{
+      h1.style.color = color;
+      resolve("color changed!");
+      
+    },delay);
+  });
+}
+colorchange("red",1000)
+.then(()=>{
+  console.log("red color was completed");
+  return colorchange("orange",1000);
+})
+.then(()=>{
+  console.log("orange color was completed");
+  return colorchange("green",1000);
+})
+.then(()=>{
+  console.log("green color was completed");
+  return colorchange("pink",1000);
+})
+.then(()=>{
+  console.log("pink color was completed");
+  
+});
+
+
+
 //the promises object represent the eventual completion or failure od asyncronous operation and its resultion value.
 //following code create confusion so that to overcome this we have another method which was implimented
 //refactoring with promises -->> or by promises chaining
@@ -80,17 +110,17 @@
 // );
 
 //refactoring with promises (promisesw are objects they return value in two way rejected or resolve )\
-function savetoDb(data){
-  return new Promise((success,failure)=>{
-    let internetSpeed = Math.floor(Math.random()*10)+1;
-    if(internetSpeed > 4){
-      success("success:data stored");
-    }else{
-      failure("failure:weak connection");
-    }
+// function savetoDb(data){
+//   return new Promise((success,failure)=>{
+//     let internetSpeed = Math.floor(Math.random()*10)+1;
+//     if(internetSpeed > 4){
+//       success("success:data stored");
+//     }else{
+//       failure("failure:weak connection");
+//     }
 
-  });
-}
+//   });
+// }
 //inpute savetoDb("apna college");
 
 
@@ -111,34 +141,33 @@ function savetoDb(data){
 
 //promise chainning-->> call hell se more readable hai ye method
 
-savetoDb("apna college")
-  .then((result)=>{
-    console.log("promise1: was saved");
-    console.log("result of promise:",result);
+// savetoDb("apna college")
+//   .then((result)=>{
+//     console.log("promise1: was saved");
+//     console.log("result of promise:",result);
 
-    return savetoDb("gargii");
-  })
-  .then((result)=>{
-    console.log("promise2: was saved");
-    console.log("result of promise:",result);
+//     return savetoDb("gargii");
+//   })
+//   .then((result)=>{
+//     console.log("promise2: was saved");
+//     console.log("result of promise:",result);
  
-    return savetoDb("gayatri");
+//     return savetoDb("gayatri");
     
-  })
-  .then((result)=>{
-    console.log("promise3: was saved");
-    console.log("result of promise:",result);
-  })
+//   })
+//   .then((result)=>{
+//     console.log("promise3: was saved");
+//     console.log("result of promise:",result);
+//   })
 
-  .catch((error)=>{
-    console.log("promise was rejected");
-    console.log("result of promise:",error);
-    
-
-    
-  });
+//   .catch((error)=>{
+//     console.log("promise was rejected");
+//     console.log("result of promise:",error);
+//   });
 
   //promises are reject or accepted with some data
+
+
 
 
 
