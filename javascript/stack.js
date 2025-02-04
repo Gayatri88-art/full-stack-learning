545ca7dfb7535f6ad4ab2179ff2680de90bbaa13
Gayatri88-art <gayatriivarade@gmail.com>
2025-02-04 09:17:36 +0530

    refactoring with promises

--- javascript/stack.js
+++ javascript/stack.js
@@ -40,38 +40,60 @@
 // });
 
 //the promises object represent the eventual completion or failure od asyncronous operation and its resultion value.
-function savetoDb(data,success,failure){
-  let internetSpeed = Math.floor(Math.random()*10)+1;
-  if(internetSpeed>4){
-    success();
-  }else{
-    failure();
-  }
+//following code create confusion so that to overcome this we have another method which was implimented
+//refactoring with promises
+
+// function savetoDb(data,success,failure){
+//   let internetSpeed = Math.floor(Math.random()*10)+1;
+//   if(internetSpeed>4){
+//     success();
+//   }else{
+//     failure();
+//   }
+// }
+// savetoDb(
+//   "hiiiii",
+//   ()=>{
+//     console.log("success:data saved");
+//     savetoDb(
+//       "word",
+//       ()=>{
+//         console.log("success2:data saved");
+//         savetoDb(
+//           "gargii",
+//           ()=>{
+//             consol.log("success3:data saved");
+//           },
+//           ()=>{
+//             console.log("failure3:weak connection");
+//           },
+//         );
+//       },
+//       ()=>{
+//         console.log("failure2:weak  connection");
+//       },
+//     );
+//   },
+//   ()=>{
+//     console.log(" failure:weak connection");
+//   },
+// );
+
+//refactoring with promises (promisesw are objects they return value in two way rejected or resolve )\
+function savetoDb(data){
+  return new Promise((success,failure)=>{
+    let internetSpeed = Math.floor(Math.random()*10)+1;
+    if(internetSpeed > 4){
+      success("success:data stored");
+    }else{
+      failure("failure:weak connection");
+    }
+
+  });
 }
-savetoDb(
-  "hiiiii",
-  ()=>{
-    console.log("success:data saved");
-    savetoDb(
-      "word",
-      ()=>{
-        console.log("success2:data saved");
-        savetoDb(
-          "gargii",
-          ()=>{
-            consol.log("success3:data saved");
-          },
-          ()=>{
-            console.log("failure3:weak connection");
-          },
-        );
-      },
-      ()=>{
-        console.log("failure2:weak  connection");
-      },
-    );
-  },
-  ()=>{
-    console.log(" failure:weak connection");
-  },
-);
+//inpute savetoDb("apna college");
+
+
+
+
+
