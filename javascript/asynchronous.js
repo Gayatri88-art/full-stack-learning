471eed5ecd8f317a0e74cbdff3e25616025ebb43
Gayatri88-art <gayatriivarade@gmail.com>
2025-02-05 21:55:13 +0530

    asynchronouss keyword

--- javascript/asynchronous.js
+++ javascript/asynchronous.js
@@ -1,1 +1,27 @@
 //day-->>26
+
+ //async keyword:=create an async function
+ //by default ye promise return karta hai
+
+
+async function greet() {
+ // throw "404 page not found";
+  return"hellow";
+}
+greet()
+.then((result)=>{
+  console.log("promised was resolved");
+  console.log("result was:",result);
+})
+.catch((err)=>{
+  console.log("promise was rejected",err);
+});
+
+let demo = async ()=>{
+  return 5;
+};
+     
+
+
+
+
