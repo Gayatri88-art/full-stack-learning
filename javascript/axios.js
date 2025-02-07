bc01efc866f06f2648dd503677fcfdb90d211172
Gayatri88-art <gayatriivarade@gmail.com>
2025-02-07 09:01:06 +0530

    sending headers with api request

--- javascript/axios.js
+++ javascript/axios.js
@@ -28,25 +28,41 @@
 
 //to print random pictures of dog
 
-let url = "https://dog.ceo/api/breeds/image/random";
+// let url = "https://dog.ceo/api/breeds/image/random";
 
-let btn = document.querySelector("button");
-btn.addEventListener("click", async()=>{
-  let link = await getImage();
-  let img = document.querySelector("#result");
-  img.setAttribute("src",link);
-  console.log(link);
-});
+// let btn = document.querySelector("button");
+// btn.addEventListener("click", async()=>{
+//   let link = await getImage();
+//   let img = document.querySelector("#result");
+//   img.setAttribute("src",link);
+//   console.log(link);
+// });
  
-  async function getImage() {
-    try{
-      let res = await axios.get(url);
-      return res.data.message;
+//   async function getImage() {
+//     try{
+//       let res = await axios.get(url);
+//       return res.data.message;
 
-    }
-    catch(e){
-      console.log("error-", e);
-      return "no img found";
-    }
+//     }
+//     catch(e){
+//       console.log("error-", e);
+//       return "no img found";
+//     }
+//   }
+
+//sending headers with APi request
+
+const url = "https://icanhazdadjoke.com/";
+ 
+async function getJokes(){
+  try{
+    const config = {Headers:{Accept:"application/json"}};
+    let res = await axios.get(url,config);
+    console.log(res.data);
+
   }
+  catch(err){
+    console.log(err);
+  }
+}
 
