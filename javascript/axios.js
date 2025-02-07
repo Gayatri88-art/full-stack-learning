73eb345a7df93a7428700221e934dd9b8fef50e7
Gayatri88-art <gayatriivarade@gmail.com>
2025-02-07 08:39:11 +0530

    get random img

--- javascript/axios.js
+++ javascript/axios.js
@@ -1,24 +1,52 @@
 //axios ==>> library to make HTTP request
-let url = "https://catfact.ninja/fact";
+//to print randome facts about cats
+// let url = "https://catfact.ninja/fact";
+
+// let btn = document.querySelector("button");
+// btn.addEventListener("click", async()=>{
+//   let fact = await getFacts();
+//   console.log(fact);
+//   let p = document.querySelector("#result");
+//   p.innerText = fact;
+
+ 
+//   async function getFacts() {
+//     try{
+//       let res = await axios.get(url);
+//       return res.data.fact;
+
+//     }
+//     catch(e){
+//       console.log("error-", e);
+//       return "no fact found";
+//     }
+//   }
+
+// })
+
+
+
+//to print random pictures of dog
+
+let url = "https://dog.ceo/api/breeds/image/random";
 
 let btn = document.querySelector("button");
 btn.addEventListener("click", async()=>{
-  let fact = await getFacts();
-  console.log(fact);
-  let p = document.querySelector("#result");
-  p.innerText = fact;
-
+  let link = await getImage();
+  let img = document.querySelector("#result");
+  img.setAttribute("src",link);
+  console.log(link);
+});
  
-  async function getFacts() {
+  async function getImage() {
     try{
       let res = await axios.get(url);
-      return res.data.fact;
+      return res.data.message;
 
     }
     catch(e){
       console.log("error-", e);
-      return "no fact found";
+      return "no img found";
     }
   }
 
-})
