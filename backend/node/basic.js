d03a87eb2fedea7b7d99e831974be7dbcef392e3
Gayatri88-art <gayatriivarade@gmail.com>
2025-02-13 07:58:32 +0530

    process in node

---
+++ backend/node/basic.js
@@ -0,0 +1,20 @@
+//node.js is runtime environment
+//it is used for server side programming
+// node.js is not a language,library ,frame work
+
+// Node REPL -->> READ
+//                 EVALUATE
+//                 print
+//                 LOOP
+
+
+// to run nodde files in terminal we use folloing commands
+//  node filename.js
+
+let n = 5;
+for(i=1; i<=5;i++){
+  console.log("hellow",i);
+}
+console.log("bye!");
+
+//process in node
