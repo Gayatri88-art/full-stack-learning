31585ee1d6b1a37a7e7483908569e4c60b66a6a9
Gayatri88-art <gayatriivarade@gmail.com>
2025-02-18 07:49:07 +0530

    what is express?

---
+++ backend/express/express.js
@@ -0,0 +1,14 @@
+
+
+// ## library ==>> library is the collection of pre-written codes that can be used  to perform a specific task
+// ex - axios
+// ## framework ==>> framework is set of pre-written codes that provieds  structurs for devloping software application
+// ex - express
+
+// express -->> express is a Node.js application framework that help us to make web application 
+//           it is used for server side programming
+//         working:-
+//           1)listen for incoming request
+//           2)pass request 
+//           3)match response with routes
+//           4)response
