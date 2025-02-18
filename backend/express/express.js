accd53e48541257fbd3a02f4519d6736cb605486
Gayatri88-art <gayatriivarade@gmail.com>
2025-02-18 08:15:49 +0530

    express packages and listrning request on serverside

--- backend/express/express.js
+++ backend/express/express.js
@@ -12,3 +12,21 @@
 //           2)pass request 
 //           3)match response with routes
 //           4)response
+
+// i perform (npm init) to initialize my directory
+
+//npm install express
+
+ const express = require("express");
+ const app = express();
+ 
+ let port = 3000;
+ app.listen(port , ()=>{
+  console.log(`app is listening on port ${port}`);
+ })
+
+//  poer -->>A port in computer networking is a communication endpoint. It acts as a logical channel through which data is exchanged between devices over a network.
+
+// Key Points:
+// Ports are identified by numbers (e.g., 80, 443, 3000).
+// Each port number is associated with a specific process or service.
