6e2e5851b792b5f2f5c4b943b8335ee12fc4d8af
Gayatri88-art <gayatriivarade@gmail.com>
2025-02-19 13:45:44 +0530

    what is templeting

---
+++ backend/ejsdir/ejs.js
@@ -0,0 +1,6 @@
+//ejs -->>  embedded javascript templetes
+//ejs is the simple templeting language which helpmus to create html markup with plan js
+
+//npm init -y
+//npm install express
+//npm install ejs
