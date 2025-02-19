30aa8eb612a5d2cb53d5fe25efbc3be1cd20748b
Gayatri88-art <gayatriivarade@gmail.com>
2025-02-19 15:59:36 +0530

    using ejs

--- backend/ejsdir/index.js
+++ backend/ejsdir/index.js
@@ -1,10 +1,18 @@
+const { render } = require("ejs");
 const express = require("express");
 const app = express();
 
 const port = 8080;
 
+app.set("view engine","ejs");
+app.get("/",(req,res)=>{
+  res.render("home.ejs"); // render is useful to send files on server
+}); 
+
 app.listen(port,()=>{
   console.log(`listening on port ${port}`)
 });
+
+
 //as we globally install nodemon
 //nodemon index.js
