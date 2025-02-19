5042087cf19cb0048e070a2ff2ac02704dc94658
Gayatri88-art <gayatriivarade@gmail.com>
2025-02-19 16:10:39 +0530

    views directory

--- backend/ejsdir/index.js
+++ backend/ejsdir/index.js
@@ -1,10 +1,12 @@
 const { render } = require("ejs");
 const express = require("express");
 const app = express();
-
+const path = require("path");
 const port = 8080;
 
 app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
 app.get("/",(req,res)=>{
   res.render("home.ejs"); // render is useful to send files on server
 }); 
