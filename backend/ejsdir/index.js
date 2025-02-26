b885673c0019fcd25fad7126ee1d9e2b751b7e94
Gayatri88-art <gayatriivarade@gmail.com>
2025-02-26 08:53:38 +0530

    instagram ejs

--- backend/ejsdir/index.js
+++ backend/ejsdir/index.js
@@ -11,6 +11,11 @@ app.get("/",(req,res)=>{
   res.render("home.ejs"); // render is useful to send files on server
 }); 
 
+app.get("/ig/:username",(req,res)=>{
+  let {username} = req.params;
+  res.render("instagram.ejs",{username});
+})
+
 app.get("/rolldice",(req,res)=>{
   let num = Math.floor(Math.random()*6)+1
   res.render("rolldice",{num}); // render is useful to send files on server
