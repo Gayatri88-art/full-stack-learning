d4d195189c11dc36080d1e162d713f7baac54624
Gayatri88-art <gayatriivarade@gmail.com>
2025-02-26 21:43:09 +0530

    handling POST request

--- miscellaneous/backend/index.js
+++ miscellaneous/backend/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({extended:true})); //handling post request -->>1)set up Post request to get some responnse
+                                                                           //2)parse POST request date 
 app.get("/register",(req,res)=>{
   let {user}=req.query;
   res.send(`standard GET response ${user}`);
@@ -10,8 +12,10 @@ app.get("/register",(req,res)=>{
 
 app.post("/register",(req,res)=>{
   res.send("standard POST response");
+  console.log(req.body);
 });
 
 app.listen(port,()=>{
   console.log(`listening to port ${port}`);
 })
+
