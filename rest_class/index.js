b1eae2a183d582c35a15b98f8984c80f1171e98b
Gayatri88-art <gayatriivarade@gmail.com>
2025-02-28 18:39:57 +0530

    creating restful apis

--- rest_class/index.js
+++ rest_class/index.js
@@ -1,21 +1,40 @@
+// REST(Representational State Transfer)
+// REST is an architectural style that defines a set of constraints to be used for creating web services
+ 
+// CRUD Operations:
+// GET retrieves resources.
+// POST submits new data to the server
+// PUT updates existing data
+// PATCH update existing data partially
+// DELETE removes data
+
+// Creating RESTful APIs
+
+// GET /posts to get data for all posts
+// POST /posts to add a new post
+// GET /posts/:id   to get one post (using id)
+// PATCH /posts/:id   to update specific post
+// DELETE /posts/:id   to delete specific post
+
 const express = require("express");
-const app = express();
-const port = 8080;
 const path = require("path");
 
-app.use(express.urlencoded({extended:true})); //so that express can understand
+const app = express();
+const port = 8080;
 
-app.set("view engine" , "ejs")
-app.set("view", path.join(__dirname,"views"));
+app.use(express.urlencoded({ extended: true })); //Parses incoming form data.
 
-app.set("express.static", path.join(__dirname,"public"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); //fixed syntax
 
-app.get("/",(req,res)=>{
+app.use(express.static(path.join(__dirname, "public"))); //fixed syntax
+
+app.get("/", (req, res) => {
   res.send("server working well!");
-})
+});
 
- 
-app.listen(port,()=>{
-  console.log("listening to port:8080");
-})
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
+
 
